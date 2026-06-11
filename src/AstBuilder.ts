da94eaf202fa4d7c051454545752cd83f72a0a9b
TwoD97 <denys_tudosa@yahoo.com>
2026-06-11 08:36:56 +0200

    Fertig Ast

--- src/AstBuilder.ts
+++ src/AstBuilder.ts
@@ -1,7 +1,7 @@
 // Baut aus einer RPN-Liste den Termbaum (AST) auf – mithilfe des Stacks.
 import { Stack } from "./Stack.js";
-import type { Token } from "./types.js";
-import type { ASTNode } from "./types.js";
+import type { Token, ASTNode } from "./types.js";
+
 
 export class AstBuilder {
     astBuild(input: Token[]): ASTNode {
@@ -11,11 +11,13 @@ export class AstBuilder {
         while (i < input.length) {
             if (input[i].kind === "number") {
                 nodes.push({ value: input[i].value });
-            } else if (input[i].kind === "operator"){
+            } else if (input[i].kind === "operator") {
                 const right = nodes.pop();
                 const left = nodes.pop()
-                nodes.push({ value: input[i].value, leftChild: left, rightChild: right})
+                nodes.push({ value: input[i].value, leftChild: left, rightChild: right })
             }
+            i++;
         }
+        return nodes.pop();
     }
 }
--- tests/astBuilder.test.ts
+++ tests/astBuilder.test.ts
@@ -3,7 +3,40 @@
 
 import { describe, it, expect } from "bun:test";
 import { AstBuilder } from "../src/AstBuilder.js";
+import { ASTNode } from "../src/types.js";
+import { Tokenizer } from "../src/Tokenizer.js";
+import { ShuntingYard } from "../src/ShuntingYard.js";
+
+function execute(term: string): ASTNode {
+  const tokens = new Tokenizer().tokenize(term);
+  const rpn = new ShuntingYard().rpnsort(tokens);
+  const tree = new AstBuilder().astBuild(rpn);
+  return tree;
+
+}
 
 describe("AstBuilder", () => {
-  it.todo("erzeugt aus '3 4 2 * 1 5 - / +' den korrekten Termbaum");
+  it("erzeugt aus ' 3 + 4' den korrekten Termbaum", () => {
+    expect(execute("3 + 4")).toEqual({
+      value: "+",
+      leftChild: { value: "3" },
+      rightChild: { value: "4" }
+    })
+  });
+  it("erzeugt aus '3 + 4 * ( 3 + 4 )' den korrekten Termbaum", () => {
+    expect(execute("3 + 4 * ( 3 + 4 )")).toEqual({
+      value: "+",
+      leftChild: { value: "3" },
+      rightChild: {
+        value: "*",
+        leftChild: {value: "4"},
+        rightChild: {
+          value: "+",
+          leftChild: {value: "3"},
+          rightChild: {value: "4"}
+        }
+      }
+
+    })
+  });
 });
